729f4c6fbe6e11405354af987edfedc16868426c
Harsh Kumar <developer.harsh101@gmail.com>
2020-04-30 04:19:55 +0530

    Location Trails showing on map

--- app/src/main/java/com/example/locationmapper/MainActivity.java
+++ app/src/main/java/com/example/locationmapper/MainActivity.java
@@ -1,16 +1,167 @@
 package com.example.locationmapper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    private static final String TAG ="ERROR" ;
+    Button mgetotpButton, mloginButton, mloginSkipButton;
+     EditText mphoneEditText, mOtpEditText;
+//    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    String phoneNumber;
+    private String codeSent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // getting view ids for buttons
+        mgetotpButton =findViewById(R.id.get_otp);
+        mloginButton =findViewById(R.id.login_btn);
+        mloginSkipButton =findViewById(R.id.skip_btn);
+        mphoneEditText=findViewById(R.id.phone);
+        mOtpEditText =findViewById(R.id.otp);
+
+
+        // login skip button listener
+        mloginSkipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mgetotpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    sendVerificationCode();
+              }
+        });
+
+        mloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String otp=mOtpEditText.getText().toString();
+
+                if(otp.isEmpty()){
+                    mOtpEditText.setError("Please enter OTP");
+                }else{
+                verifySignInCode();
+            }
+
+
+
+            }
+        });
+
     }
+
+    private void verifySignInCode(){
+        String code = mOtpEditText.getText().toString();
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //here you can open new activity
+                            Log.i("LOGIN","Login successfull");
+                            Toast.makeText(getApplicationContext(),
+                                    "Login Successfull", Toast.LENGTH_LONG).show();
+                            Intent newintent=new Intent(MainActivity.this,MapsActivity.class);
+                            startActivity(newintent);
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Incorrect Verification Code ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+
+    private void sendVerificationCode(){
+
+        String phone = mphoneEditText.getText().toString();
+
+        if(phone.isEmpty()){
+            mphoneEditText.setError("Phone number is required");
+            mphoneEditText.requestFocus();
+            return;
+        }
+
+        if(phone.length() < 10 ){
+            mphoneEditText.setError("Please enter a valid phone");
+            mphoneEditText.requestFocus();
+            return;
+        }
+
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phone,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    };
+
+
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            codeSent = s;
+        }
+    };
+
+
+
 }
--- app/src/main/java/com/example/locationmapper/MapsActivity.java
+++ app/src/main/java/com/example/locationmapper/MapsActivity.java
@@ -2,7 +2,16 @@ package com.example.locationmapper;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,20 +19,42 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
+    private final long MIN_TIME = 3000;        //time to update location = 3 seconds
+    private final long MIN_DISTANCE = 1;    //1 meter displacement
+
+    Polyline polyline = null;
+    private ArrayList<LatLng> locationTrail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
+
+        locationTrail = new ArrayList<>();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
+
 
 
     /**
@@ -35,13 +66,125 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // adding polylines
+        final PolylineOptions polylineOptions = new PolylineOptions()
+                .addAll(locationTrail).clickable(true);
+
+        polyline = googleMap.addPolyline(polylineOptions);
+        polyline.setWidth(10f);
+
+        Log.d("DEBUG", "MAP READY");
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                Log.d("DEBUG", "UPDATED");
+
+                LatLng updated = new LatLng(location.getLatitude(), location.getLongitude());
+
+                // adding new updated location
+                locationTrail.add(updated);
+
+                //adding marker
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String timeStamp  = dateFormat.format(new Date());
+
+                mMap.addMarker(new MarkerOptions().position(updated).title("Timestamp: " + timeStamp));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(updated,24.0f));
+
+                //adding new polyline
+                polylineOptions.add(updated);
+                polyline = mMap.addPolyline(polylineOptions);
+                polyline.setColor(Color.rgb(74,137,243));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        //requesting location updates for location listener
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Toast.makeText(this, "GPS is Enabled in your device", Toast.LENGTH_SHORT).show();
+
+        }else{
+            // GPS not enabled
+            Toast.makeText(this, "GPS is not enabled in your device", Toast.LENGTH_SHORT).show();
+            showGPSDisabledAlertToUser();
+        }
+
+        //setting location update time and distance
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener);
+
+
+            Log.d("DEBUG", "first");
+
+            // first marker
+            Location loc = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            LatLng updated = new LatLng(loc.getLatitude(), loc.getLongitude());
+
+            // adding new updated location
+            locationTrail.add(updated);
+
+            //adding marker
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String timeStamp  = dateFormat.format(new Date());
+
+            mMap.addMarker(new MarkerOptions().position(updated).title("Timestamp: " + timeStamp));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(updated,24.0f));
+
+            //adding new polyline
+            polylineOptions.add(updated);
+            polyline = mMap.addPolyline(polylineOptions);
+            polyline.setColor(Color.rgb(74,137,243));
+
+        } catch (SecurityException | NullPointerException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled in your device please enable it.")
+                .setCancelable(false)
+                .setPositiveButton("Goto Settings Page To Enable GPS",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
